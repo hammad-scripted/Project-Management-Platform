52d9faf48a9b2c679d256024aa412c49bbbb9778
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-05-03 18:24:58 +0530

    add ApiError and ApiResponse utility classes

---
+++ src/utils/api-response.js
@@ -0,0 +1,10 @@
+class ApiResponse {
+  constructor(statusCode, data = null, message = "success") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.error = null;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
+}
+export default ApiResponse;
