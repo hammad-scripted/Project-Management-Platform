4cc0768e61bbbba246497de2a9ed5aa5f343b67e
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-05-03 18:48:52 +0530

    add mongoose for database connection and implement connection logic

---
+++ src/db/connection.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+export const connectToDb = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URL);
+    return connection;
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+export default connectToDb;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
+import dns from "node:dns/promises";
+dns.setServers(["1.1.1.1"]);
 import app from "./app.js";
 import chalk from "chalk";
+import connectToDb from "./db/connection.js";
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(chalk.magenta(`Server is listening on ${PORT}...`));
-});
+connectToDb()
+  .then((connection) => {
+    console.log(chalk.bgBlack.yellow("Connected to DB Successfully"));
+    app.listen(PORT, () => {
+      console.log(
+        chalk.bgMagentaBright.blackBright.bold(
+          `Server is listening on ${PORT}...`,
+        ),
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(chalk.red("Failed to connect to DB"), error);
+    process.exit(1);
+  });
