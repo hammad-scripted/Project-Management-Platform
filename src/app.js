d13cb54720a293a00ba6b7f65e6019338100fa1d
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-05-03 18:11:27 +0530

    add CORS middleware and update dependencies

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 import express from "express";
-
+import cors from "cors";
 const app = express();
+
+//** MIDDLEWARES */
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    optionsSuccessStatus: 200,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+//** ROUTES */
+
 app.get("/", (req, res) => {
   res.send("hello");
 });
