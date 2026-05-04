bfd821da2a93dfdd4893af5185ffc7567060693b
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-05-04 22:46:24 +0530

    add bcrypt for password hashing and implement pre-save hook in user model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -63,4 +63,14 @@ const userSchema = new Schema(
 );
 
 const User = mongoose.model('User', userSchema);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  } else {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  }
+});
 export default User;
